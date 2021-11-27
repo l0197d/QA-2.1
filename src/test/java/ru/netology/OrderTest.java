3525ae00dcc3c2efbbd5c02c5c1bc5fe2b470200
l0197d <titus04@yandex.ru>
2021-11-27 18:01:51 +0300

    v5.2

--- src/test/java/ru/netology/OrderTest.java
+++ src/test/java/ru/netology/OrderTest.java
@@ -13,50 +13,43 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class OrderTest {
+    private WebDriver driver;
 
-    public void setDriver() {
-        System.setProperty("webdriver.chrome.driver", "J:\\Project\\chromedriver.exe");
-        WebDriver wd =new ChromeDriver();
-        String baseUrl = "http://localhost:9999/";
-        wd.get(baseUrl);
-
-
-    }
-    WebDriver driver= new ChromeDriver();
     @BeforeAll
-    static void setUpAll() {WebDriverManager.chromedriver().setup(); }
+    static void setUpAll() {
+        WebDriverManager.chromedriver().setup();
+    }
 
-        @BeforeEach
-        public void setupTest() {
-            ChromeOptions options = new ChromeOptions();
-            options.addArguments("start-maximized");
-            options.addArguments("disable-infobars");
-            options.addArguments("--headless");
-            options.addArguments("--disable-extensions");
-            options.addArguments("--disable-dev-shm-usage");
-            options.addArguments("--no-sandbox");
- //           driver = new ChromeDriver(options);
+    @BeforeEach
+    public void setupTest() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        options.addArguments("disable-infobars");
+        options.addArguments("--headless");
+        options.addArguments("--disable-extensions");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        driver = new ChromeDriver(options);
+    }
+
+    @AfterEach
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
         }
+    }
 
-
-        @AfterEach
-        public void teardown() {
-            if (driver != null) {
-                driver.quit();
-            }
-        }
-        @Test
-        void shouldSubmitRequest() {
-            driver.get("http://localhost:9999");
-            driver.findElement(By.cssSelector("[type='text']")).sendKeys("Иванов Петр");
-            driver.findElement(By.cssSelector("[type='tel']")).sendKeys("+71231234567");
-            driver.findElement(By.cssSelector(".checkbox__box")).click();
-            driver.findElement(By.cssSelector(".button")).click();
-            String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
-            String actual = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText();
-            assertEquals(expected, actual.strip());
-        }
-
+    @Test
+    void shouldSubmitRequest() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[type='text']")).sendKeys("Иванов Петр");
+        driver.findElement(By.cssSelector("[type='tel']")).sendKeys("+71231234567");
+        driver.findElement(By.cssSelector(".checkbox__box")).click();
+        driver.findElement(By.cssSelector(".button")).click();
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText();
+        assertEquals(expected, actual.strip());
+    }
 
     @Test
     void shouldWriteWrongName() {
@@ -98,14 +91,7 @@ public class OrderTest {
         String actual = driver.findElement(By.cssSelector(".input_invalid .checkbox__text")).getText();
         assertEquals(expected, actual.strip());
     }
-
-
-
-
-    }
-
-
-
+}
 
 
 
