e7818bc86b9816b97697009bd75dfb7415775e70
l0197d <titus04@yandex.ru>
2021-12-15 13:50:31 +0300

    Переход на Selenide

--- src/test/java/ru/netology/OrderTest.java
+++ src/test/java/ru/netology/OrderTest.java
@@ -1,98 +1,70 @@
 package ru.netology;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import com.codeborne.selenide.Condition;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import com.codeborne.selenide.ElementsCollection;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.*;
 
 public class OrderTest {
-    private WebDriver driver;
-
-    @BeforeAll
-    static void setUpAll() {
-        WebDriverManager.chromedriver().setup();
-    }
-
-    @BeforeEach
-    public void setupTest() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("start-maximized");
-        options.addArguments("disable-infobars");
-        options.addArguments("--headless");
-        options.addArguments("--disable-extensions");
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--no-sandbox");
-        driver = new ChromeDriver(options);
-    }
-
-    @AfterEach
-    public void teardown() {
-        if (driver != null) {
-            driver.quit();
-        }
-    }
-
     @Test
     void shouldSubmitRequest() {
-        driver.get("http://localhost:9999");
-        driver.findElement(By.cssSelector("[type='text']")).sendKeys("Иванов Петр");
-        driver.findElement(By.cssSelector("[type='tel']")).sendKeys("+71231234567");
-        driver.findElement(By.cssSelector(".checkbox__box")).click();
-        driver.findElement(By.cssSelector(".button")).click();
-        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
-        String actual = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText();
-        assertEquals(expected, actual.strip());
+        open("http://localhost:9999");
+        ElementsCollection inputs = $$ (".input__control");
+        inputs.find(Condition.attribute("type","text")).setValue("Иванов Петр");
+        inputs.find(Condition.attribute("type","tel")).setValue("+71234567890");
+        $(".checkbox__box").click();
+        $(".button").click();
+        $("[data-test-id='order-success']").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+    @Test
+    void LatinName() {
+        open("http://localhost:9999");
+        ElementsCollection inputs = $$ (".input__control");
+        inputs.find(Condition.attribute("type","text")).setValue("Ivanov Petr");
+        $(".button").click();
+        $(".input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+    @Test
+    void shouldWriteNumberTwelveSymbols(){
+        open("http://localhost:9999");
+        ElementsCollection inputs = $$ (".input__control");
+        inputs.find(Condition.attribute("type","text")).setValue("Иванов Петр");
+        inputs.find(Condition.attribute("type","tel")).setValue("+790926809090");
+        $(".checkbox__box").click();
+        $(".button").click();
+        $("[data-test-id='phone'] .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
 
     @Test
-    void shouldWriteWrongName() {
-        driver.get("http://localhost:9999");
-        driver.findElement(By.cssSelector("[type='text']")).sendKeys("Ivanov Petr");
-        driver.findElement(By.cssSelector(".button")).click();
-        String expected = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
-        String actual = driver.findElement(By.cssSelector(".input__sub")).getText();
-        assertEquals(expected, actual.strip());
+    void shouldWriteNumberWithoutPlus() {
+        open("http://localhost:9999");
+        ElementsCollection inputs = $$ (".input__control");
+        inputs.find(Condition.attribute("type","text")).setValue("Иванов Петр");
+        inputs.find(Condition.attribute("type","tel")).setValue("89092680909");
+        $(".checkbox__box").click();
+        $(".button").click();
+        $("[data-test-id='phone'] .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+    @Test
+    void shouldFieldsEmpty(){
+        open("http://localhost:9999");
+        $(".button").click();
+        $(".input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
     }
 
     @Test
-    void shouldWriteWrongNumber() {
-        driver.get("http://localhost:9999");
-        driver.findElement(By.cssSelector("[type='text']")).sendKeys("Иванов Петр");
-        driver.findElement(By.cssSelector("[type='tel']")).sendKeys("81231234567");
-        driver.findElement(By.cssSelector(".button")).click();
-        String expected = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
-        String actual = driver.findElement(By.cssSelector("[data-test-id='phone'] .input__sub")).getText();
-        assertEquals(expected, actual.trim());
-    }
-
-    @Test
-    void shouldFieldsEmpty() {
-        driver.get("http://localhost:9999");
-        driver.findElement(By.cssSelector(".button")).click();
-        String expected = "Поле обязательно для заполнения";
-        String actual = driver.findElement(By.cssSelector(".input_invalid .input__sub")).getText();
-        assertEquals(expected, actual.trim());
-    }
-
-    @Test
-    void shouldCheckboxEmpty() {
-        driver.get("http://localhost:9999");
-        driver.findElement(By.cssSelector("[type='text']")).sendKeys("Иванов Петр");
-        driver.findElement(By.cssSelector("[type='tel']")).sendKeys("+71231234567");
-        driver.findElement(By.cssSelector(".button")).click();
-        String expected = "Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй";
-        String actual = driver.findElement(By.cssSelector(".input_invalid .checkbox__text")).getText();
-        assertEquals(expected, actual.strip());
+    void shouldCheckboxEmpty(){
+        open("http://localhost:9999");
+        ElementsCollection inputs = $$ (".input__control");
+        inputs.find(Condition.attribute("type","text")).setValue("Иванов Петр");
+        inputs.find(Condition.attribute("type","tel")).setValue("+79092680909");
+        $(".button").click();
+        $(".input_invalid .checkbox__text").shouldHave(exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй"));
     }
 }
-
 
 
 
