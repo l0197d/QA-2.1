a84ebcb164b2fd99af5f88837255e3adf401d2f1
IBelalov <ilia.belalov@hermesrussia.ru>
2021-12-17 14:27:02 +0300

    1 more negative test, mistakes.

--- src/test/java/ru/netology/OrderTest.java
+++ src/test/java/ru/netology/OrderTest.java
@@ -2,16 +2,25 @@ package ru.netology;
 
 import com.codeborne.selenide.Condition;
 
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.ElementsCollection;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class OrderTest {
-    @Test
-    void shouldSubmitRequest() {
+
+    @BeforeEach
+    void setup ()
+    {
         open("http://localhost:9999");
+    }
+    @Test
+    //Positive test
+    void shouldSubmitRequest() {
+      //  Configuration.holdBrowserOpen = true;
         ElementsCollection inputs = $$ (".input__control");
         inputs.find(Condition.attribute("type","text")).setValue("Иванов Петр");
         inputs.find(Condition.attribute("type","tel")).setValue("+71234567890");
@@ -20,16 +29,18 @@ public class OrderTest {
         $("[data-test-id='order-success']").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
     @Test
+        //Negative test
     void LatinName() {
-        open("http://localhost:9999");
+       // Configuration.holdBrowserOpen = true;
         ElementsCollection inputs = $$ (".input__control");
         inputs.find(Condition.attribute("type","text")).setValue("Ivanov Petr");
         $(".button").click();
         $(".input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
     @Test
+        //Negative test
     void shouldWriteNumberTwelveSymbols(){
-        open("http://localhost:9999");
+        //Configuration.holdBrowserOpen = true;
         ElementsCollection inputs = $$ (".input__control");
         inputs.find(Condition.attribute("type","text")).setValue("Иванов Петр");
         inputs.find(Condition.attribute("type","tel")).setValue("+790926809090");
@@ -39,8 +50,9 @@ public class OrderTest {
     }
 
     @Test
+        //Negative test
     void shouldWriteNumberWithoutPlus() {
-        open("http://localhost:9999");
+        //Configuration.holdBrowserOpen = true;
         ElementsCollection inputs = $$ (".input__control");
         inputs.find(Condition.attribute("type","text")).setValue("Иванов Петр");
         inputs.find(Condition.attribute("type","tel")).setValue("89092680909");
@@ -49,20 +61,34 @@ public class OrderTest {
         $("[data-test-id='phone'] .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
     @Test
+        //Negative test
     void shouldFieldsEmpty(){
-        open("http://localhost:9999");
+        //Configuration.holdBrowserOpen = true;
         $(".button").click();
         $(".input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+
     }
 
     @Test
+        //Negative test
     void shouldCheckboxEmpty(){
-        open("http://localhost:9999");
+       // Configuration.holdBrowserOpen = true;
         ElementsCollection inputs = $$ (".input__control");
         inputs.find(Condition.attribute("type","text")).setValue("Иванов Петр");
         inputs.find(Condition.attribute("type","tel")).setValue("+79092680909");
         $(".button").click();
         $(".input_invalid .checkbox__text").shouldHave(exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй"));
+    }
+
+    @Test
+        //Negative test
+    void shouldWithOutPhoneNumber() {
+       // Configuration.holdBrowserOpen = true;
+        ElementsCollection inputs = $$ (".input__control");
+        inputs.find(Condition.attribute("type","text")).setValue("Иванов Петр");
+        $(".button").click();
+        $(".checkbox__box").click();
+        $(".input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
     }
 }
 
