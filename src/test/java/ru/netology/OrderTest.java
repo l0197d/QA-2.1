a9bc9b209ebd650bc918151f3c9301e84602cb43
l0197d <titus04@yandex.ru>
2021-12-18 20:18:43 +0300

    Update test, mistakes

--- src/test/java/ru/netology/OrderTest.java
+++ src/test/java/ru/netology/OrderTest.java
@@ -21,51 +21,48 @@ public class OrderTest {
     //Positive test
     void shouldSubmitRequest() {
       //  Configuration.holdBrowserOpen = true;
-        ElementsCollection inputs = $$ (".input__control");
-        inputs.find(Condition.attribute("type","text")).setValue("Иванов Петр");
-        inputs.find(Condition.attribute("type","tel")).setValue("+71234567890");
+        $("[data-test-id='name'] input").setValue("Иванов Петр");
+        $("[data-test-id='phone'] input").setValue("+71234567890");
         $(".checkbox__box").click();
         $(".button").click();
         $("[data-test-id='order-success']").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
     @Test
         //Negative test
-    void LatinName() {
+    void  shouldGetErrorWithLatinName() {
        // Configuration.holdBrowserOpen = true;
-        ElementsCollection inputs = $$ (".input__control");
-        inputs.find(Condition.attribute("type","text")).setValue("Ivanov Petr");
+        $("[data-test-id='name'] input").setValue("Ivanov Petr");
+        $("[data-test-id='phone'] input").setValue("+71234567890");
         $(".button").click();
-        $(".input_invalid .input__sub").shouldHave(exactText("Имя и Фамилия указаны неверно. Допустимы только русские буквы, пробелы и дефисы."));
+        $("[data-test-id='name'].input_invalid .input__sub").shouldHave(exactText("Имя и Фамилия указаны неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
     @Test
         //Negative test
     void shouldWriteNumberTwelveSymbols(){
         //Configuration.holdBrowserOpen = true;
-        ElementsCollection inputs = $$ (".input__control");
-        inputs.find(Condition.attribute("type","text")).setValue("Иванов Петр");
-        inputs.find(Condition.attribute("type","tel")).setValue("+790926809090");
+        $("[data-test-id='name'] input").setValue("Иванов Петр");
+        $("[data-test-id='phone'] input").setValue("+790926809090");
         $(".checkbox__box").click();
         $(".button").click();
-        $(".input_invalid .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+        $("[data-test-id='phone'].input_invalid .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
 
     @Test
         //Negative test
     void shouldWriteNumberWithoutPlus() {
-        //Configuration.holdBrowserOpen = true;
-        ElementsCollection inputs = $$ (".input__control");
-        inputs.find(Condition.attribute("type","text")).setValue("Иванов Петр");
-        inputs.find(Condition.attribute("type","tel")).setValue("89092680909");
+
+        $("[data-test-id='name'] input").setValue("Иванов Петр");
+        $("[data-test-id='phone'] input").setValue("81234567890");
         $(".checkbox__box").click();
         $(".button").click();
-        $(".input_invalid .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+        $("[data-test-id='phone'].input_invalid .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
     @Test
         //Negative test
     void shouldFieldsEmpty(){
         //Configuration.holdBrowserOpen = true;
         $(".button").click();
-        $(".input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+        $("[data-test-id='name'].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
 
     }
 
@@ -73,23 +70,32 @@ public class OrderTest {
         //Negative test
     void shouldCheckboxEmpty(){
        // Configuration.holdBrowserOpen = true;
-        ElementsCollection inputs = $$ (".input__control");
-        inputs.find(Condition.attribute("type","text")).setValue("Иванов Петр");
-        inputs.find(Condition.attribute("type","tel")).setValue("+79092680909");
+        $("[data-test-id='name'] input").setValue("Иванов Петр");
+        $("[data-test-id='phone'] input").setValue("+71234567890");
         $(".button").click();
-        $(".input_invalid .checkbox__text").shouldHave(exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй"));
+        $("[data-test-id='agreement'].input_invalid .checkbox__text").shouldHave(exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй"));
     }
 
     @Test
         //Negative test
     void shouldWithOutPhoneNumber() {
        // Configuration.holdBrowserOpen = true;
-        ElementsCollection inputs = $$ (".input__control");
-        inputs.find(Condition.attribute("type","text")).setValue("Иванов Петр");
+        $("[data-test-id='name'] input").setValue("Иванов Петр");
         $(".button").click();
         $(".checkbox__box").click();
-        $(".input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+        $("[data-test-id='phone'].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
     }
+
+    @Test
+        //Negative test
+    void shouldWithOutName() {
+        //Configuration.holdBrowserOpen = true;
+        $("[data-test-id='phone'] input").setValue("+71234567890");
+        $(".button").click();
+        $(".checkbox__box").click();
+        $("[data-test-id='name'].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+    }
+
 }
 
 
